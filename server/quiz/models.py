77c3e038a8a2cbe54423c2944c62951a6ec9151c
wilfried90 <ngangouanzokou@gmail.com>
2025-06-11 04:17:08 +0200

    modeles du projet

--- server/quiz/models.py
+++ server/quiz/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+
+class Result(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    user = models.CharField(max_length=100)
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.score}'
